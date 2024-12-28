5d750ac302fc7193ae6a50b716c714016d6b4a3e
Dushmanta <dushmanta0511@gmail.com>
2024-12-28 18:15:37 +0530

    feat: implement express

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Backend running on ${port}`);
+});
